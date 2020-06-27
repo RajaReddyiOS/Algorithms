2e8c889a2353fc3a6b50dd8e29fdcc08a99ca618
Raja Reddy <raja@cogcons.com>
2020-06-27 08:43:16 +0530

    * String Reverse in different methods

--- StringReversal.playground/Contents.swift
+++ StringReversal.playground/Contents.swift
@@ -2,7 +2,8 @@
 
 import Foundation
 
-func reverse(of str: String) -> String {
+//MARK: - Method - 1
+func reverse_1(of str: String) -> String {
     var strList = Array(str)
     strList = strList.reversed()
     var reversedStr = ""
@@ -12,4 +13,33 @@ func reverse(of str: String) -> String {
     return reversedStr
 }
 
-print("reversing string: ", reverse(of: "sos"))
+//MARK: - Method - 2
+func reverse_2(of str: String) -> String {
+    var reversed = ""
+    for char in Array(str) {
+        reversed = String(char)+reversed
+    }
+    return reversed
+}
+
+//MARK: - Method - 3
+func reverse_3(of str: String) -> String {
+    let strList = Array(str)
+    var reversed = ""
+    for index in stride(from: strList.count, to: 0, by: -1) {
+        reversed+=String(strList[index-1])
+    }
+    return reversed
+}
+
+//MARK: - Method - 4
+func reverse_4(of str: String) -> String {
+    return Array(str).reduce("", { result, char in
+        return String(char) + String(result)
+    })
+}
+
+print("reversing string method_1: ", reverse_1(of: "12345"))
+print("reversing string method_2: ", reverse_2(of: "12345"))
+print("reversing string method_3: ", reverse_3(of: "12345"))
+print("reversing string method_3: ", reverse_4(of: "12345"))
